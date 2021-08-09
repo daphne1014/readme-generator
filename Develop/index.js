3cd6dde9b7b67e81f04303d95e27b4cb5bb49244
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-08-09 00:19:19 -0700

    added array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,96 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+const questions = () => {
+    return inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'title',
+          message: 'What is your project title?',
+          validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your project title!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Provide some description of your project',
+          validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please provide some description!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'installation',
+          message: 'What are the steps required to install your project?',
+          validate: nameInput => {
+            if (nameInput) {
+                return true;
+              } else {
+                console.log('Please provide installation description!');
+                return false;
+              }
+        }
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What are usage information when using this app?',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please provide instructions and examples for use!');
+              return false;
+            }
+      }
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Who were the contributers to this project?',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please provide contributer infomation!');
+              return false;
+            }
+      }
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Go the extra mile and write tests for your application.',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please provide contributer infomation!');
+              return false;
+            }
+      }
+    }
+      ])
+  };
+  
+  questions();
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
