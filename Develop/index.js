6a60f0f1d9bec8e17d9ac7df48415d101fbc2d2e
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-08-14 01:49:00 -0700

    added license selection

--- Develop/index.js
+++ Develop/index.js
@@ -6,45 +6,45 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
 // const questions = [];
 const questions = () => {
-    return inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'title',
-          message: 'What is your project title?',
-          validate: nameInput => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log('Please enter your project title!');
-              return false;
-            }
+  return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your project title!');
+            return false;
           }
-        },
-        {
-          type: 'input',
-          name: 'description',
-          message: 'Provide some description of your project',
-          validate: nameInput => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log('Please provide some description!');
-              return false;
-            }
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide some description of your project',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please provide some description!');
+            return false;
           }
-        },
-        {
-          type: 'input',
-          name: 'installation',
-          message: 'What are the steps required to install your project?',
-          validate: nameInput => {
-            if (nameInput) {
-                return true;
-              } else {
-                console.log('Please provide installation description!');
-                return false;
-              }
+        }
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please provide installation description!');
+            return false;
+          }
         }
       },
       {
@@ -52,51 +52,76 @@ const questions = () => {
         name: 'usage',
         message: 'What are usage information when using this app?',
         validate: nameInput => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log('Please provide instructions and examples for use!');
-              return false;
-            }
-      }
-    },
-    {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please provide instructions and examples for use!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Which licenses are you adding to this project? (Check all that apply)',
+        choices: ['Apache 2.0 License', 'Boost Software License 1.0', 'BSD 3-Clause License', 'Eclipse Public License 1.0', 'GNU GPL v3', 'The MIT License', 'Open Database License (ODbL)']
+      },
+      {
         type: 'input',
         name: 'credits',
         message: 'Who were the contributers to this project?',
         validate: nameInput => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log('Please provide contributer infomation!');
-              return false;
-            }
-      }
-    },
-    {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please provide contributer infomation!');
+            return false;
+          }
+        }
+      },
+      {
         type: 'input',
         name: 'tests',
         message: 'Go the extra mile and write tests for your application.',
         validate: nameInput => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log('Please provide contributer infomation!');
-              return false;
-            }
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please provide contributer infomation!');
+            return false;
+          }
+        }
       }
-    }
-      ])
-  };
-  
-  questions();
+    ])
+};
+
+questions();
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(fileName, data, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'README File created!'
+      });
+    });
+  });
+};
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
+
+
+module.exports = { writeToFile};
