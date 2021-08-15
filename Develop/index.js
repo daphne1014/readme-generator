6e7511605e3d42ed4a03ddf85c4479c6cf7be051
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-08-14 20:37:19 -0700

    completed readme generator

--- Develop/index.js
+++ Develop/index.js
@@ -4,125 +4,122 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-// const questions = [];
-const questions = () => {
-  return inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'title',
-        message: 'What is your project title?',
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('Please enter your project title!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Provide some description of your project',
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('Please provide some description!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: 'What are the steps required to install your project?',
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('Please provide installation description!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'What are usage information when using this app?',
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('Please provide instructions and examples for use!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'Which licenses are you adding to this project?',
-        choices: ['Apache 2.0 License', 'Boost Software License 1.0', 'BSD 3-Clause License', 'Eclipse Public License 1.0', 'GNU GPL v3', 'The MIT License', 'Open Database License (ODbL)']
-      },
-      {
-        type: 'input',
-        name: 'credits',
-        message: 'Who were the contributers to this project?',
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('Please provide contributer infomation!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'tests',
-        message: 'Go the extra mile and write tests for your application.',
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('Please provide contributer infomation!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'username',
-        message: 'What is your GitHub Username?',
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('Please provide your GitHub Username!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?',
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('Please provide your email address!');
-            return false;
-          }
-        }
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter your project title!');
+        return false;
       }
-    ])
+    }
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide some description of your project',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please provide some description!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the steps required to install your project?',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please provide installation description!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What are usage information when using this app?',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please provide instructions and examples for use!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Which licenses are you adding to this project?',
+    choices: ['Apache 2.0 License', 'Boost Software License 1.0', 'BSD 3-Clause License', 'Eclipse Public License 1.0', 'GNU GPL v3', 'The MIT License', 'Open Database License (ODbL)']
+  },
+  {
+    type: 'input',
+    name: 'credits',
+    message: 'Who were the contributers to this project?',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please provide contributer infomation!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Go the extra mile and write tests for your application.',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please provide contributer infomation!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'What is your GitHub Username?',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please provide your GitHub Username!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please provide your email address!');
+        return false;
+      }
+    }
+  }
+];
+
+function promptUser() {
+  return inquirer.prompt(questions)
 };
-
-questions();
-
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -143,8 +140,23 @@ function writeToFile(fileName, data) {
 
 
 
+
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+  promptUser()
+  .then(data => {
+    return generateMarkdown(data)
+  })
+  .then(fileContent =>{
+    return writeToFile('./README.md', fileContent)
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -3,49 +3,81 @@
 function renderLicenseBadge(license) {
   if (!license) {
     return '';
-}
+  }
+  else if (license === 'Apache 2.0 License') {
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license === 'Boost Software License 1.0') {
+    return `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+  } else if (license === 'BSD 3-Clause License') {
+    return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+  } else if (license === 'Eclipse Public License 1.0') {
+    return `[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`
+  } else if (license === 'GNU GPL v3') {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  } else if (license === 'The MIT License') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } else if (license === 'Open Database License (ODbL)') {
+    return `[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)`
+  }
 };
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {
+//   if (!license) {
+//     return '';
+//   }
+//   else if (license === 'Apache 2.0 License') {
+//     return 'https://opensource.org/licenses/Apache-2.0'
+//   } else if (license === 'Boost Software License 1.0') {
+//     return 'https://www.boost.org/LICENSE_1_0.txt'
+//   } else if (license === 'BSD 3-Clause License') {
+//     return 'https://opensource.org/licenses/BSD-3-Clause'
+//   } else if (license === 'Eclipse Public License 1.0') {
+//     return 'https://opensource.org/licenses/EPL-1.0'
+//   } else if (license === 'GNU GPL v3') {
+//     return 'https://www.gnu.org/licenses/gpl-3.0'
+//   } else if (license === 'The MIT License') {
+//     return 'https://opensource.org/licenses/MIT'
+//   } else if (license === 'Open Database License (ODbL)') {
+//     return 'https://opendatacommons.org/licenses/odbl/'
+//   }
+// }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-  
-  # ${data.title} ${renderLicenseBadgeLink(data.license)}
+  return `# ${data.title} ${renderLicenseBadge(data.license)}
 
-  ##Description
+  ## Description
   ${data.description}
 
-  ##Table of Contents
+  ## Table of Contents
   - [Installation](#installation)
   - [Usage](#usage)
   - [Contributing](#contributing)
   - [Tests](#tests)
   - [Questions](#questions)
 
-  ##Installation
+  ## Installation
   ${data.installation}
 
-  ##Usage
+  ## Usage
   ${data.usage}
 
-  ##Contributing
+  ## Contributing
   ${data.credits}
 
-  ##License
+  ## License
   ${data.license}
 
-  ##Tests
+  ## Tests
   ${data.tests}
 
-  ##Questions
+  ## Questions
 
   Feel free to reach out to me on GitHub: [${data.username}](https://github.com/${data.username}).
 
